ebffcf7804c555961e54632e780649849520651e
Mark <18929562811@163.com>
2017-02-03 20:58:58 +0800

    swift练习项目第一个，基础工程

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # SwiftDailyProjects
 swift练习项目集合
+## 这个是参考30DaysofSwift开源项目，自己练习的项目，而不是简单的copy
---
+++ SwiftProjectOne/SwiftProjectOne/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  SwiftProjectOne
+//
+//  Created by 叶杨杨 on 2017/2/3.
+//  Copyright © 2017年 叶杨杨. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var endButton: UIButton!
+    @IBOutlet weak var timeLable: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
