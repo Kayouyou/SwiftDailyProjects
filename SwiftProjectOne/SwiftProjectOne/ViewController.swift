eb817dd7b199c938f68e2dd59c90730aa0663195
Mark <18929562811@163.com>
2017-02-03 21:27:41 +0800

    倒计时项目完毕

--- SwiftProjectOne/SwiftProjectOne/ViewController.swift
+++ SwiftProjectOne/SwiftProjectOne/ViewController.swift
@@ -14,16 +14,64 @@ class ViewController: UIViewController {
     @IBOutlet weak var endButton: UIButton!
     @IBOutlet weak var timeLable: UILabel!
     
+    var Counter = 0.0
+    var CountDownTimer = Timer()
+    var IsPlaying = false
+    //swift3.0 基于某一个控制器修改状态栏的颜色，重写这个属性既可以！
+    override var preferredStatusBarStyle: UIStatusBarStyle{
+        get {
+            return.lightContent
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        timeLable.text = String(Counter);
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func startTap(_ sender: UIButton) {
+    
+        if IsPlaying {
+            return
+        }
+        startButton.isEnabled = false
+        endButton.isEnabled   = true
+        CountDownTimer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        IsPlaying = true
     }
-
+    
+    @IBAction func endOrPauseTap(_ sender: Any) {
+    
+        startButton.isEnabled = true
+        endButton.isEnabled   = false
+        CountDownTimer.invalidate()
+        IsPlaying  = false
+    }
+    
+    @IBAction func resetTap(_ sender: Any) {
+    
+        CountDownTimer.invalidate()
+        IsPlaying = false;
+        Counter = 0
+        timeLable.text = String(Counter)
+        startButton.isEnabled = true;
+        endButton.isEnabled   = true;
+    }
+    
+    @objc func updateTimer() ->() {
+        
+        Counter = Counter + 0.1
+        timeLable.text = String(format:"%.1f",Counter)
+    }
 
 }
+
+// MARK:控制器扩展
+
+extension ViewController{
+    
+
+}
+
+
 
